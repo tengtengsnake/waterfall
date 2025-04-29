f38c17da008614f65718dd414ecdad233c1658a3
Teng <peitengtsai@gmail.com>
2025-04-29 18:26:52 +1000

    update

--- src/data.ts
+++ src/data.ts
@@ -1,36 +1,27 @@
-import daxing1 from '/images/daxing1.png'
-import daxing2 from '/images/daxing2.png'
-import tml1 from '/images/tml1.png'
-import casino1 from '/images/casino1.png'
-import goco2 from '/images/goco2.png'
-import fenghuang1 from '/images/fenghuang1.png'
-import fenghuang2 from '/images/fenghuang2.png'
-import fenghuang3 from '/images/fenghuang3.png'
-import hanggang from '/images/hanggang.png'
-import donglai1 from '/images/donglai1.png'
-import hubStation1 from '/images/hubStation1.png'
-import rochedale from '/images/rochedale.png'
-import wuxi1 from '/images/wuxi1.png'
-import goco from '/images/goco.png'
-import soho from '/images/soho.jpg'
-import storybridge from '/images/storybridge.png'
-import kingIsland from '/images/kingIsland.png'
-import bne1 from '/images/bne1.png'
-import jan241 from '/images/jan241.png'
-import hgh1 from '/images/hgh1.png'
-import hgh2 from '/images/hgh2.png'
-import hgh3 from '/images/hgh3.jpg'
-import hangzhoueast1 from '/images/hangzhoueast1.png'
-import hh1 from '/images/hh1.png'
-import hz5 from '/images/hz5.png'
-import hz9 from '/images/hz9.png'
-import hz365 from '/images/hz365.png'
-import intersection from '/images/intersection.png'
-import shanghai1 from '/images/shanghai1.png'
-import shanghaitower from '/images/shanghaitower.png'
-import bne254 from '/images/bne254.png'
-import banshan1 from '/images/banshan1.png'
-import sanche from '/images/sanche.png'
+import ad1 from '/images/ad1.jpg'
+import ad2 from '/images/ad2.jpg'
+import ad3 from '/images/ad3.jpg'
+import ad4 from '/images/ad4.jpg'
+import ad5 from '/images/ad5.jpg'
+import ad6 from '/images/ad6.jpg'
+import ad7 from '/images/ad7.jpg'
+import ad8 from '/images/ad8.jpg'
+import ad9 from '/images/ad9.jpg'
+import ad10 from '/images/ad10.jpg'
+import ad11 from '/images/ad11.jpg'
+import ad12 from '/images/ad12.jpg'
+import ad13 from '/images/ad13.jpg'
+import ad14 from '/images/ad14.jpg'
+import ad15 from '/images/ad15.jpg'
+import ad16 from '/images/ad16.jpg'
+import ad17 from '/images/ad17.jpg'
+import ad18 from '/images/ad18.jpg'
+import ad19 from '/images/ad19.jpg'
+import ad20 from '/images/ad20.jpg'
+import ad21 from '/images/ad21.jpg'
+import ad22 from '/images/ad22.jpg'
+import ad23 from '/images/ad23.jpg'
+
 
 export interface Theme {
   title: string
@@ -50,57 +41,47 @@ function shuffleArray<T>(array: T[]): T[] {
 
 export const themes: Theme[] = [
   {
-    title: "Urban Architecture",
-    description: "Exploring the geometric patterns and stark contrasts of modern cityscapes, where light and shadow dance across steel and glass surfaces.",
-    coverImage: soho,
+    title: "Hanok Korean BBQ",
+    description: "Example",
+    coverImage: ad1,
     gallery: shuffleArray([
-      daxing1,
-      daxing2,
-      tml1,
-      casino1,
-      goco2,
-      fenghuang1,
-      fenghuang2,
-      fenghuang3,
-      storybridge,
-      bne1,
-      hanggang,
-      intersection,
-      shanghai1,
-      shanghaitower,
+      ad1,
+      ad2,
+      ad3,
+      ad4,
+      ad5,
     ])
   },
   {
     title: "Public Transportation",
     description: "",
-    coverImage: daxing1,
+    coverImage: ad2,
     gallery: shuffleArray([
-      hubStation1,
-      hgh1,
-      hgh2,
-      hgh3,
-      hangzhoueast1,
-      hz365,
-      hz5,
-      hz9,
-      bne254,
-      hh1,
-      sanche,
-      intersection,
+     ad6,
+     ad7,
+     ad8,
+     ad9,
+     ad10
     ])
   },
   {
     title: "Nature",
     description: "",
-    coverImage: goco,
+    coverImage: ad3,
     gallery: shuffleArray([
-      goco,
-      donglai1,
-      wuxi1,
-      rochedale,
-      jan241,
-      kingIsland,
-      banshan1,
+     ad11,
+     ad12,
+     ad13,
+     ad14,
+     ad15,
+     ad16,
+     ad17,
+     ad18,
+     ad19,
+     ad20,
+     ad21,
+     ad22,
+     ad23
     ])
   }
 ]
