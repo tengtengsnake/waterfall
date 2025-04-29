47e1fc159671b42cb97a7c7b8311ace7d29550d1
tengtengsnake <63833307+tengtengsnake@users.noreply.github.com>
2025-04-29 23:31:56 +1000

    Update vite.config.ts
    Activate function about https (self sign with mkert)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     host: true, // 或直接設為你主機的 IP，例如 '0.0.0.0'
+    https: true,
     allowedHosts: ['ube.li'] // 允許來自這個 host 的請求
   },
 })
